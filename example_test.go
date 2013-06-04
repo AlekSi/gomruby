9e617bcc6e9a8ff8a8db93d0af71e8af9bcbdf8a
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-06-04 22:45:01 +0400

    Extract example into separate file.

---
+++ example_test.go
@@ -0,0 +1,44 @@
+package gomruby_test
+
+import (
+	"."
+	"fmt"
+)
+
+func ExampleFilterLogs() {
+	mruby := gomruby.New()
+	context := mruby.NewLoadContext("filter.rb")
+
+	userCode := `
+		message.include? "500"
+	`
+
+	code := fmt.Sprintf(`
+		def filter
+			message = ARGV[0]
+			%s
+		end`, userCode)
+	_, err := context.Load(code)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, message := range []string{
+		`1.2.3.1 - - [04/Jun/2013:18:02:01 +0000] host "GET /foo HTTP/1.0" 200`,
+		`1.2.3.2 - - [04/Jun/2013:18:02:02 +0000] host "GET /bar HTTP/1.0" 300`,
+		`1.2.3.3 - - [04/Jun/2013:18:02:03 +0000] host "GET /baz HTTP/1.0" 400`,
+		`1.2.3.4 - - [04/Jun/2013:18:02:04 +0000] host "GET /bzr HTTP/1.0" 500`,
+	} {
+		res, err := context.Load("filter", message)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(res.(bool))
+	}
+
+	// Output:
+	// false
+	// false
+	// false
+	// true
+}
--- gomruby_test.go
+++ gomruby_test.go
@@ -3,7 +3,6 @@ package gomruby_test
 import (
 	. "."
 	"errors"
-	"fmt"
 	"io/ioutil"
 	. "launchpad.net/gocheck"
 	"testing"
@@ -129,40 +128,4 @@ func (f *F) TestBugRb(c *C) {
 	res, err := f.c.Load(string(b))
 	c.Logf("%#v", res)
 	c.Check(err, IsNil)
-}
-
-func ExampleFilterLogs() {
-	mruby := New()
-	context := mruby.NewLoadContext("filter.rb")
-
-	userCode := `
-		message.include? "500"
-	`
-
-	_, err := context.Load(fmt.Sprintf(`
-		def filter
-			message = ARGV[0]
-			%s
-		end`, userCode))
-	if err != nil {
-		panic(err)
-	}
-	for _, message := range []string{
-		`1.2.3.1 - - [04/Jun/2013:18:02:01 +0000] host "GET /foo HTTP/1.0" 200`,
-		`1.2.3.2 - - [04/Jun/2013:18:02:02 +0000] host "GET /bar HTTP/1.0" 300`,
-		`1.2.3.3 - - [04/Jun/2013:18:02:03 +0000] host "GET /baz HTTP/1.0" 400`,
-		`1.2.3.4 - - [04/Jun/2013:18:02:04 +0000] host "GET /bzr HTTP/1.0" 500`,
-	} {
-		res, err := context.Load("filter", message)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(res.(bool))
-	}
-
-	// Output:
-	// false
-	// false
-	// false
-	// true
 }
